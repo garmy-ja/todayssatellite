91a12bcce97f7f2bc2a3830e929e8ca7ac0839e4
dnogami <admin@garmy.jp>
2017-04-01 16:41:19 +0900

    unnecessary require removed
    デバッグ中に追加した不必要なrequire文などを削除

--- bot_control.rb
+++ bot_control.rb
@@ -1,13 +1,6 @@
 #!/usr/bin/env ruby
 
-require 'pathname'
 require 'rubygems'
 require 'daemons'
 
-begin
-  Daemons.run('satellite_tweet_bot.rb')
-rescue => e
-  p e.class
-  p e.message
-  p e.backtrace
-end
+Daemons.run('satellite_tweet_bot.rb')
